2978c2f0809ec484050ccd4e3b610013474fe315
xiaomisis <109336245+xiaomisis@users.noreply.github.com>
2022-07-15 09:19:00 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -10,3 +10,14 @@ echo "# 2. Laptop config"
 
 sudo apt -qqqy install webext-ublock-origin-firefox webext-https-everywhere webext-privacy-badger
 sudo apt -qqqy install fastboot adb
+
+echo "# 3. Remove packages"
+
+PACKAGES_LIST=$(pm list packages | sed "s/^package://")
+REMOVABLE_PACKAGES=$(cat ./removable_packages.txt)
+for TAG in $REMOVABLE_PACKAGES ; do
+  for PACKAGE in $(echo $PACKAGES_LIST | grep ${TAG}); do
+    echo -n "Removing ${PACKAGE}: "
+    pm uninstall -k --user 0 $PACKAGE
+  done
+done 
