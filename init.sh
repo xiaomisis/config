bfe5ae4221097e0f77e6309f83a822f979c20ffb
xiaomisis <109336245+xiaomisis@users.noreply.github.com>
2022-07-15 09:06:07 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-echo "Factory reset] Settings > About Phone > Factory reset > Erase all data"
-echo "1. [Developer Options] Settings > About Phone > [7x] MIUI version"
-echo "2. [USB debugging] Settings > Additional settings > Developer options > USB debugging."
+echo "[Factory reset] Settings > About Phone > Factory reset > Erase all data"
+echo "[Developer Options] Settings > About Phone > [7x] MIUI version"
+echo "[USB debugging] Settings > Additional settings > Developer options > USB debugging."
