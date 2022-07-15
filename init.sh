bb6f6ed2b810c4c783ad69777003d38cfe3c5426
xiaomisis <109336245+xiaomisis@users.noreply.github.com>
2022-07-15 09:09:01 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,3 +3,6 @@
 echo "[Factory reset]     - Settings > About Phone > Factory reset > Erase all data"
 echo "[Developer Options] - Settings > About Phone > [7x] MIUI version"
 echo "[USB debugging]     - Settings > Additional settings > Developer options > USB debugging"
+
+sudo apt -y install webext-ublock-origin-firefox webext-https-everywhere webext-privacy-badger
+sudo apt-get -y install fastboot adb
